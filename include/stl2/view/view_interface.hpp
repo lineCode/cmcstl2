dd93fe1f396e30b275df15ab20a6e89d5d9b1342
Casey Carter <Casey@Carter.net>
2018-11-09 18:40:15 -0800

    view_interface: mutable empty and operator bool from P0896R4

--- include/stl2/view/view_interface.hpp
+++ include/stl2/view/view_interface.hpp
@@ -72,15 +72,26 @@ STL2_OPEN_NAMESPACE {
 			return static_cast<const D&>(*this);
 		}
 	public:
+		constexpr bool empty() requires ForwardRange<D> {
+			auto& d = derived();
+			return __stl2::begin(d) == __stl2::end(d);
+		}
 		constexpr bool empty() const requires ForwardRange<const D> {
 			auto& d = derived();
 			return __stl2::begin(d) == __stl2::end(d);
+		}
+
+		constexpr explicit operator bool()
+		// Distinct named concept to workaround https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82740
+		requires detail::CanEmpty<D> {
+			return !__stl2::empty(derived());
 		}
 		constexpr explicit operator bool() const
 		// Distinct named concept to workaround https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82740
 		requires detail::CanEmpty<const D> {
 			return !__stl2::empty(derived());
 		}
+
 		template <Range R = D>
 		requires ContiguousIterator<iterator_t<R>>
 		constexpr auto data() {
